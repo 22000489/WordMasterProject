5f2509edb775a2260b968ef2d6ffb321397fba63
22000489 <22000489@handong.ac.kr>
2022-09-06 20:03:35 +0900

    modified WordCRUD

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -28,7 +28,7 @@ public class WordCRUD implements ICRUD {
     public void addWord(){
     Word one = (Word)add();
     list.add(one);
-    System.out.println("\n새 단어가 단어장에 추가되었습니다. ");
+    System.out.println("\n새 단어가 단어장에 추가되었습니다 !!! ");
     }
 
 

7dcbfe19660f098a5ae9dfe005f9cef8eeca1d4f
22000489 <22000489@handong.ac.kr>
2022-09-06 16:52:33 +0900

    create WordManager

---
+++ src/main/java/com/mycom/word/WordManager.java
@@ -0,0 +1,47 @@
+package com.mycom.word;
+
+import java.util.Scanner;
+
+public class WordManager {
+    Scanner s = new Scanner (System.in);
+    WordCRUD wordCRUD;
+    /* word를 추가하는 addWord 함수는 실제로 WordCRUD에 있으므로
+    생성자를 만들 때 wordCRUD라는 이름으로 WordCRUD 멤버변수를 하나 선언해줌
+    */
+
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
+    public int selectMenu() {
+        System.out.print("*** 영단어 마스터 ***\n"
+                + "********************\n"
+                + "1. 모든 단어 보기\n"
+                + "2. 수준별 단어 보기\n"
+                + "3. 단어 검색\n"
+                + "4. 단어 추가\n"
+                + "5. 단어 수정\n"
+                + "6. 단어 삭제\n"
+                + "7. 파일 저장\n"
+                + "0. 나가기\n"
+                + "********************\n"
+                + "=> 원하는 메뉴는? ");
+        return s.nextInt();
+    }
+
+    public void start() {
+        while(true) {
+            int menu = selectMenu();
+//            System.out.print(menu);
+            if(menu == 0) break;
+            if(menu == 4) {
+                //create
+                wordCRUD.addWord();
+            }
+            else if(menu == 1) {
+                //list
+                wordCRUD.listAll();
+            }
+        }
+    }
+}
+
